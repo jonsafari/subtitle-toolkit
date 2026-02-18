9b9cc6b3ff543d990975892c4321403926e362b7
Jon Dehdari <jon@dehdari.org>
2026-02-17 18:53:55 -0800

    +fr txn

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 TRANSLATIONS_DIR = Path("translations")
 
 # Available languages
-AVAILABLE_LANGUAGES = ["en", "es", "de"]
+AVAILABLE_LANGUAGES = ["en", "es", "de", "fr"]
 DEFAULT_LANGUAGE = "en"
 
 def load_translations(language: str) -> dict:
